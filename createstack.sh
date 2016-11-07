0c2acc51ed5b8d38eb5c45c08385e2401a77c427
Peter Kumaschow <pkumaschow@gmail.com>
2016-11-08 01:47:11 +1100

    renamed cfn template

--- createstack.sh
+++ createstack.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-aws cloudformation create-stack --template-body file://./phaselist.yml --stack-name phaselist-dev --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --template-body file://./phaselist.template --stack-name phaselist-dev --capabilities CAPABILITY_NAMED_IAM
